063138f8a6c5f9310413ffe7acf7c64703f8d167
unknown <muziko11@hotmail.com>
2020-05-22 18:01:35 -0500

    POST

--- Controllers/tareasController.php
+++ Controllers/tareasController.php
@@ -137,6 +137,129 @@ elseif (empty($_GET)) {
             exit();
         }
     }
+    elseif($_SERVER['REQUEST_METHOD'] === 'POST'){
+        try {
+            if ($_SERVER['CONTENT_TYPE'] !== 'application/json'){
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                $response->addMessage('Encabezado "Content type" no es JSON');
+                $response->send();
+                exit();
+            }
+
+            $postData = file_get_contents('php://input');
+
+            if (!$json_data = json_decode($postData)) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                $response->addMessage('El cuerpo de la solicitud no es un JSON válido');
+                $response->send();
+                exit();
+            }
+
+            if (!isset($json_data->titulo) || !isset($json_data->completada) || !isset($json_data->categoria_id)) {
+                $response = new Response();
+                $response->setHttpStatusCode(400);
+                $response->setSuccess(false);
+                (!isset($json_data->titulo) ? $response->addMessage('El título es obligatorio') : false);
+                (!isset($json_data->completada) ? $response->addMessage('El campo de completada es obligatorio') : false);
+                (!isset($json_data->ticategoria_idtulo) ? $response->addMessage('La categoría es obligatoria') : false);
+                $response->send();
+                exit();
+            }
+
+            $tarea = new Tarea(
+                null, 
+                $json_data->titulo,
+                (isset($json_data->descripcion) ? $json_data->descripcion : null),
+                (isset($json_data->fecha_limite) ? $json_data->fecha_limite : null),
+                $json_data->completada,
+                $json_data->categoria_id
+            );
+
+            $titulo = $tarea->getTitulo();
+            $descripcion = $tarea->getDescripcion();
+            $fecha_limite = $tarea->getFechaLimite();
+            $completada = $tarea->getCompletada();
+            $categoria_id = $tarea->getCategoriaID();
+
+            $query = $connection->prepare('INSERT INTO tareas (titulo, descripcion, fecha_limite, completada, categoria_id) VALUES (:titulo, :descripcion, STR_TO_DATE(:fecha_limite, \'%Y-%m-%d %H:%i\'), :completada, :categoria_id)');
+            $query->bindParam(':titulo', $titulo, PDO::PARAM_STR);
+            $query->bindParam(':descripcion', $descripcion, PDO::PARAM_STR);
+            $query->bindParam(':fecha_limite', $fecha_limite, PDO::PARAM_STR);
+            $query->bindParam(':completada', $completada, PDO::PARAM_STR);
+            $query->bindParam(':categoria_id', $categoria_id, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Error al crear la tarea");
+                $response->send();
+                exit();
+            }
+
+            $ultimo_ID = $connection->lastInsertId();
+
+            $query = $connection->prepare('SELECT id, titulo, descripcion, DATE_FORMAT(fecha_limite, "%Y-%m-%d %H:%i") fecha_limite, completada, categoria_id FROM tareas WHERE id = :id');
+            $query->bindParam(':id', $ultimo_ID, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            if ($rowCount === 0) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage("Error al obtener tarea después de crearla");
+                $response->send();
+                exit();
+            }
+
+            $tareas = array();
+
+            while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                $tarea = new Tarea($row['id'], $row['titulo'], $row['descripcion'], $row['fecha_limite'], $row['completada'], $row['categoria_id']);
+
+                $tareas[] = $tarea->getArray();
+            }
+
+            $returnData = array();
+            $returnData['total_registros'] = $rowCount;
+            $returnData['tareas'] = $tareas;
+
+            $response = new Response();
+            $response->setHttpStatusCode(201);
+            $response->setSuccess(true);
+            $response->addMessage("Tarea creada");
+            $response->setData($returnData);
+            $response->send();
+            exit();
+        }
+        catch (TareaException $e) {
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage($e->getMessage());
+            $response->send();
+            exit();
+        }
+        catch (PDOException $e){
+            error_log("Error en BD - " . $e);
+
+            $response = new Response();
+            $response->setHttpStatusCode(500);
+            $response->setSuccess(false);
+            $response->addMessage("Error en creación de tareas");
+            $response->send();
+            exit();
+        }
+    }
     else {
         $response = new Response();
         $response->setHttpStatusCode(405);
